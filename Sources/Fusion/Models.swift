00a7b25885bcd4913ec1e6f6ee88a207e9792db2
neptune84x <neptune84x0@gmail.com>
2026-05-03 15:37:00 +0300

    Remove isSelected from QueueItem struct
    Removed isSelected property from QueueItem struct.

--- Sources/Fusion/Models.swift
+++ Sources/Fusion/Models.swift
@@ -8,7 +8,5 @@ struct QueueItem: Identifiable, Equatable {
     let id = UUID()
     let url: URL
     var status: JobStatus = .waiting
-    var isSelected: Bool = false
-    
     var filename: String { url.lastPathComponent }
 }
